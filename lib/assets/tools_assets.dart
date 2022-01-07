ba75d1fb08b60c2b67ab61a9dcfc7dfc371493ec
Aidyl <jgarciaalbalah@gmail.com>
2022-01-07 15:23:15 -0500

    + ToolsAssets class

---
+++ lib/assets/tools_assets.dart
@@ -0,0 +1,13 @@
+class ToolsAssets {
+  static const _assets = "packages/citmatel_strawberry_tools/assets";
+  static const _icons = "$_assets/icons";
+  static const _audios = "$_assets/audios";
+
+  ////ICONS////
+  static const String ICON_BABY_BOY = "$_icons/baby_boy.png";
+  static const String ICON_BABY_GIRL = "$_icons/baby_girl.png";
+
+  ////AUDIOS///
+  static const String AUDIO_CORRECT = "$_audios/keyboard_correct.wav";
+  static const String AUDIO_WRONG = "$_audios/keyboard_error.mp3";
+}
