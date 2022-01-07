c888760be28933f99ddc80eb61ede4de77442afa
Aidyl <jgarciaalbalah@gmail.com>
2022-01-07 15:23:52 -0500

    StrawberryAudio: + import ToolsAssets

--- lib/src/a_utils/strawberry_audio.dart
+++ lib/src/a_utils/strawberry_audio.dart
@@ -1,17 +1,15 @@
 import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 
 class StrawberryAudio {
-  static const String AUDIO_CORRECT = "packages/citmatel_strawberry_tools/assets/audios/keyboard_correct.wav";
-  static const String AUDIO_WRONG = "packages/citmatel_strawberry_tools/assets/audios/keyboard_error.mp3";
-
   final assetsAudioPlayer = AssetsAudioPlayer();
 
   static void playAudioCorrect() {
-    playAudio(AUDIO_CORRECT);
+    playAudio(ToolsAssets.AUDIO_CORRECT);
   }
 
   static void playAudioWrong() {
-    playAudio(AUDIO_WRONG);
+    playAudio(ToolsAssets.AUDIO_WRONG);
   }
 
   static void playAudio(String audioUrl) {
