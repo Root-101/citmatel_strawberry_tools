8e101a6f99b9fee18bed5975401ccca968223736
Aidyl <jgarciaalbalah@gmail.com>
2022-01-07 15:26:33 -0500

    Exporter updated

---
+++ lib/src/b_screens/screens_exporter.dart
@@ -0,0 +1,4 @@
+library screen_exporter;
+
+export 'strawberry_level_win.dart';
+export 'strawberry_level_lose.dart';
--- lib/tools_exporter.dart
+++ lib/tools_exporter.dart
@@ -1,3 +1,5 @@
 library tools_exporter;
 
 export 'src/a_utils/utils_exporter.dart';
+export 'src/b_screens/screens_exporter.dart';
+export 'assets/tools_assets.dart';
