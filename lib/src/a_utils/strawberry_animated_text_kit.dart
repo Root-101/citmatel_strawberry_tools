efea5280d49d4254efb038a2b54caf0ddf6e7316
Aidyl <jgarciaalbalah@gmail.com>
2022-01-10 16:52:08 -0500

    StrawberryAnimatedTextKit: +colorizeAnimatedText & rotateAnimatedText

--- lib/src/a_utils/strawberry_animated_text_kit.dart
+++ lib/src/a_utils/strawberry_animated_text_kit.dart
@@ -9,35 +9,63 @@ class StrawberryAnimatedTextKit {
     Colors.red,
   ];
 
-  static Widget congratsAnimatedText() {
+  static Widget colorizeAnimatedText({
+    required String text,
+    double fontSize = 70.0,
+    List<MaterialColor> listOfColors = colorizeColors,
+    bool repeatForever = false,
+    TextAlign textAlign = TextAlign.center,
+  }) {
     return AnimatedTextKit(
       animatedTexts: [
+        // The effect for the AnimatedTextKit is going to be ColorizeAnimatedText.
         ColorizeAnimatedText(
-          'Felicidades',
-          textStyle: const TextStyle(
-            fontSize: 70.0,
-            fontFamily: 'Horizon',
+          // The text to show.
+          text,
+          textStyle: TextStyle(
+            // The size of the font.
+            fontSize: fontSize,
           ),
-          colors: colorizeColors,
+          // To align the text to the center.
+          textAlign: textAlign,
+          // The list of colors to change between.
+          colors: listOfColors,
         ),
       ],
+      // Repeat the text rotate effect forever true or false.
       repeatForever: true,
     );
   }
 
-  static Widget congratsLongAnimatedText() {
+  static Widget rotateAnimatedText({
+    required List<String> texts,
+    double fontSize = 50.0,
+    Color color = Colors.blueAccent,
+    bool repeatForever = false,
+    TextAlign textAlign = TextAlign.center,
+  }) {
     return DefaultTextStyle(
-      style: const TextStyle(
-        fontSize: 50.0,
-        color: Colors.blueAccent,
+      style: TextStyle(
+        // The size of the font.
+        fontSize: fontSize,
+        // The color of the font
+        color: color,
       ),
       child: AnimatedTextKit(
-        animatedTexts: [
-          RotateAnimatedText('Has Ganado'),
-          RotateAnimatedText('Lo Lograste'),
-          RotateAnimatedText('Eres el Mejor'),
-        ],
-        repeatForever: true,
+        // Generates as meny RotateAnimatedText as are in the texts list.
+        animatedTexts: List.generate(
+          // Amount of items in the text list.
+          texts.length,
+          // The effect for the AnimatedTextKit is going to be RotateAnimatedText.
+          (index) => RotateAnimatedText(
+            // The text to show.
+            texts[index],
+            // To align the text to the center.
+            textAlign: textAlign,
+          ),
+        ),
+        // Repeat the text rotate effect forever true or false.
+        repeatForever: repeatForever,
       ),
     );
   }
