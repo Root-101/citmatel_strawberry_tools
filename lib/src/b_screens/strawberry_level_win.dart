ab1f38d001c63f82bf61c332f82baf600282319d
Aidyl <jgarciaalbalah@gmail.com>
2022-01-10 15:55:35 -0500

    StrawberryLevelWin: + StrawberryAnimatedTextKit

--- lib/src/b_screens/strawberry_level_win.dart
+++ lib/src/b_screens/strawberry_level_win.dart
@@ -1,11 +1,26 @@
 import 'package:animated_background/animated_background.dart';
-import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:citmatel_strawberry_tools/src/a_utils/strawberry_animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class StrawberryLevelWin extends StatefulWidget {
   static const ROUTE_NAME = "/tools-win-level-screen";
 
-  const StrawberryLevelWin({Key? key}) : super(key: key);
+  late Widget _childFirstText =
+      StrawberryAnimatedTextKit.congratsAnimatedText();
+  late Widget _childSecondText =
+      StrawberryAnimatedTextKit.congratsLongAnimatedText();
+
+  StrawberryLevelWin(
+      {Key? key, Widget? childFirstText, Widget? childSecondText})
+      : super(key: key) {
+    if (childFirstText != null) {
+      _childFirstText = childFirstText;
+    }
+    if (childSecondText != null) {
+      _childSecondText = childSecondText;
+    }
+  }
 
   @override
   State<StrawberryLevelWin> createState() => _StrawberryLevelWinState();
@@ -15,17 +30,6 @@ class _StrawberryLevelWinState extends State<StrawberryLevelWin>
     with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    const colorizeColors = [
-      Colors.purple,
-      Colors.blue,
-      Colors.yellow,
-      Colors.red,
-    ];
-
-    const colorizeTextStyle = TextStyle(
-      fontSize: 70.0,
-      fontFamily: 'Horizon',
-    );
     return Scaffold(
       body: AnimatedBackground(
         behaviour: RandomParticleBehaviour(
@@ -45,32 +49,8 @@ class _StrawberryLevelWinState extends State<StrawberryLevelWin>
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              SizedBox(
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    ColorizeAnimatedText(
-                      'Felicidades',
-                      textStyle: colorizeTextStyle,
-                      colors: colorizeColors,
-                    ),
-                  ],
-                  repeatForever: true,
-                ),
-              ),
-              DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 50.0,
-                  color: Colors.blueAccent,
-                ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    RotateAnimatedText('Has Ganado'),
-                    RotateAnimatedText('Lo Lograste'),
-                    RotateAnimatedText('Eres el Mejor'),
-                  ],
-                  repeatForever: true,
-                ),
-              ),
+              widget._childFirstText,
+              widget._childSecondText,
             ],
           ),
         ),
