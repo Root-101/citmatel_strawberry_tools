aeb35b31792c25cf87222c8c107f2712e9250a71
Aidyl <jgarciaalbalah@gmail.com>
2022-01-11 22:57:40 -0500

    StrawberryLevelWin: + ToolsAssetsHappyBrain & ToolsAssetsTrofie

--- lib/src/b_screens/strawberry_level_win.dart
+++ lib/src/b_screens/strawberry_level_win.dart
@@ -1,5 +1,6 @@
 import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animator/flutter_animator.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 // ignore: must_be_immutable
@@ -7,7 +8,7 @@ class StrawberryLevelWin extends StatefulWidget {
   static const ROUTE_NAME = "/tools-win-level-screen";
 
   late Widget _childFirstText = StrawberryAnimatedTextKit.colorizeAnimatedText(
-    text: 'Felicidades',
+    text: ' Felicidades',
     repeatForever: true,
   );
 
@@ -48,20 +49,26 @@ class _StrawberryLevelWinState extends State<StrawberryLevelWin>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //// THE RIGHT BUTTON ////
       floatingActionButton: StrawberryWidgets.circularButtonWithIcon(
+        // The function to display when the rigth button is pressed.
         onPressed: widget.rightButtonFunction,
         child: StrawberryWidgets.pulseIconAnimation(
+          // The icon of the button with an animation.
           icon: Icons.home_sharp,
         ),
       ),
       body: Container(
-        decoration:  BoxDecoration(
+        decoration: BoxDecoration(
+          // The background color.
           color: Colors.black,
           image: DecorationImage(
+            // If it exits a background image.
             image: widget._backgroundImage,
             fit: BoxFit.cover,
           ),
         ),
+        //// THE ANIMATED BACKGROUND ////
         child: StrawberryWidgets.animateBackground(
           vsync: this,
           child: StrawberryWidgets.animateBackground(
@@ -69,6 +76,58 @@ class _StrawberryLevelWinState extends State<StrawberryLevelWin>
             baseColor: Colors.red,
             child: Stack(
               children: [
+                //// THE IMAGE OF THE TROFIE ////
+                Padding(
+                  padding: const EdgeInsets.only(top: 70),
+                  child: Tada(
+                    preferences: const AnimationPreferences(
+                        autoPlay: AnimationPlayStates.Loop),
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            ToolsAssetsTrofie.randomTrofie(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //// THE ANIMATED TEXT ////
+                Padding(
+                  padding: const EdgeInsets.only(top: 290),
+                  child: Stack(
+                    children: [
+                      widget._childFirstText,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 40),
+                        child: widget._childSecondText,
+                      ),
+                    ],
+                  ),
+                ),
+                //// THE IMAGE OF THE BRAIN ////
+                Padding(
+                  padding: const EdgeInsets.only(top: 490, left: 25),
+                  child: Bounce(
+                    preferences: const AnimationPreferences(
+                        autoPlay: AnimationPlayStates.Loop),
+                    child: Container(
+                      alignment: Alignment.bottomCenter,
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            ToolsAssetsHappyBrain.randomHappyBrain(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //// THE LEFT BUTTON ////
                 Container(
                   alignment: Alignment.bottomLeft,
                   padding: const EdgeInsets.all(15),
@@ -78,14 +137,6 @@ class _StrawberryLevelWinState extends State<StrawberryLevelWin>
                       icon: Icons.next_plan_outlined,
                     ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    widget._childFirstText,
-                    widget._childSecondText,
-                  ],
                 ),
               ],
             ),
