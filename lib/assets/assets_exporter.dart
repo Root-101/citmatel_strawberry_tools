505207fc83411144167d1ca493f8c5ed827c1f0b
Aidyl <jgarciaalbalah@gmail.com>
2022-01-21 17:10:29 -0500

    + class ToolsAssetsBrainLearn

--- lib/assets/assets_exporter.dart
+++ lib/assets/assets_exporter.dart
@@ -4,3 +4,4 @@ export 'tools_assets.dart';
 export 'images/sad_brain/tools_assets_sad_brain.dart';
 export 'images/happy_brain/tools_assets_happy_brain.dart';
 export 'images/trofie/tools_assets_trofie.dart';
+export 'images/brain_learn/tools_assets_brain_learn.dart';
