cf2976b116c7a75ac2c2b00041328d97f01edd48
JHB <jhernandezb96@gmail.com>
2022-01-27 00:16:13 -0500

    +ToolsThemesBackgroundImage

---
+++ lib/assets/images/themes/tools_themes_background_image.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class ToolsThemesBackgroundImage {
+  final String urlImage;
+  final Color colorStrong;
+  final Color colorLight;
+
+  ToolsThemesBackgroundImage({
+    required this.urlImage,
+    required this.colorStrong,
+    required this.colorLight,
+  });
+}
