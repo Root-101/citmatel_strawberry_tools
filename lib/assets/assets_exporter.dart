c3437538a0ff15fb5709ff55fda81399761882d6
JHB <jhernandezb96@gmail.com>
2022-01-27 00:16:46 -0500

    fix images/themes exporter

--- lib/assets/assets_exporter.dart
+++ lib/assets/assets_exporter.dart
@@ -1,7 +1,8 @@
-library utils_exporter;
+library tools_assets_exporter;
 
-export 'tools_assets.dart';
-export 'images/sad_brain/tools_assets_sad_brain.dart';
-export 'images/happy_brain/tools_assets_happy_brain.dart';
-export 'images/trofie/tools_assets_trofie.dart';
 export 'images/brain_learn/tools_assets_brain_learn.dart';
+export 'images/happy_brain/tools_assets_happy_brain.dart';
+export 'images/sad_brain/tools_assets_sad_brain.dart';
+export 'images/themes/tools_themes_exporter.dart';
+export 'images/trofie/tools_assets_trofie.dart';
+export 'tools_assets.dart';
---
+++ lib/assets/images/themes/tools_themes_exporter.dart
@@ -0,0 +1,4 @@
+library tools_assets_theme_exporter;
+
+export 'tools_themes_assets.dart';
+export 'tools_themes_background_image.dart';
