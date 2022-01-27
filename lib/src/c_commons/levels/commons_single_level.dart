44378de08f93a2ccbfd2392acec23bebab3a7aa3
JHB <jhernandezb96@gmail.com>
2022-01-26 23:57:10 -0500

    CommonsSingleLevel: +SliverFab

--- lib/src/c_commons/levels/commons_single_level.dart
+++ lib/src/c_commons/levels/commons_single_level.dart
@@ -1,67 +1,93 @@
+import 'dart:ui';
+
+import 'package:background_app_bar/background_app_bar.dart';
+import 'package:citmatel_strawberry_tools/src/a_utils/strawberry_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:sliver_fab/sliver_fab.dart';
 
 class CommonsSingleLevel<SubLevelDomain> extends StatelessWidget {
-  final String moduleName;
+  final List<SubLevelDomain> subLevelsAll;
+
   final String themeTitle;
+  final String urlThemePicture;
+
   final int maxStars;
   final int winedStars;
 
-  final Function(SubLevelDomain sublevel) singleLevelBuilder;
-
-  final List<SubLevelDomain> subLevelsAll;
-
-  final String urlThemePicture;
+  final Function(SubLevelDomain subLevel) singleSubLevelTileBuilder;
 
   final int crossAxisCount;
 
   const CommonsSingleLevel({
-    required this.moduleName,
     required this.themeTitle,
     required this.maxStars,
     required this.winedStars,
     required this.subLevelsAll,
-    required this.singleLevelBuilder,
     required this.urlThemePicture,
+    required this.singleSubLevelTileBuilder,
     this.crossAxisCount = 2,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(urlThemePicture),
-          fit: BoxFit.cover,
+    double expandedHeight = MediaQuery.of(context).size.height * 0.3;
+    return Scaffold(
+      body: SliverFab(
+        floatingWidget: StrawberryWidgets.circularButtonWithIcon(
+          onPressed: () {},
+          child: StrawberryWidgets.pulseIconAnimation(
+              icon: Icons.radio_button_checked_sharp),
         ),
-      ),
-      child: CustomScrollView(
-        slivers: [
+        floatingPosition: const FloatingPosition(right: 16),
+        expandedHeight: expandedHeight,
+        slivers: <Widget>[
           SliverAppBar(
-            actions: [
-              //estrellas
-              _buildStars(),
-            ],
-            /*expandedHeight: MediaQuery.of(context).size.height * 0.3,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                color: Colors.red,
-              ),
-            ),*/
-            //para que salga cuando se escrolea hacia arriba aunque no haya llegado al principio
+            expandedHeight: expandedHeight,
             floating: true,
-            //titulo
-            title: Text(themeTitle),
-            //centrar el titulo
-            centerTitle: true,
-            //boton de regresar
+            pinned: true,
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
             leading: const BackButton(color: Colors.white),
+            flexibleSpace: BackgroundFlexibleSpaceBar(
+              collapseMode: CollapseMode.pin,
+              title: Text(
+                themeTitle,
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              centerTitle: true,
+              titlePadding: const EdgeInsets.only(left: 20.0, bottom: 20.0),
+              background: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        urlThemePicture,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
           SliverGrid.count(
             crossAxisCount: crossAxisCount,
             children: subLevelsAll
                 .map(
-                  (e) => Function.apply(singleLevelBuilder, [e]) as Widget,
+                  (e) =>
+                      Function.apply(singleSubLevelTileBuilder, [e]) as Widget,
                 )
                 .toList(),
           ),
