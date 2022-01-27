7be5e45018b1967beb1d6859064f7272082caa77
JHB <jhernandezb96@gmail.com>
2022-01-26 23:57:38 -0500

    commons_levels_exporter: +*levels

--- lib/src/c_commons/levels/commons_levels_exporter.dart
+++ lib/src/c_commons/levels/commons_levels_exporter.dart
@@ -1,6 +1,7 @@
 library commons_levels_exporter;
 
 //export all the levels widgets
-export 'commons_levels_screen.dart';
+export 'commons_levels_theme_screen.dart';
+export 'commons_levels_theme_single_tile.dart';
 export 'commons_single_level.dart';
 export 'commons_single_sublevel_tile.dart';
