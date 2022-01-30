6bcc199e478036423a02ccb604cc9db94155a7ca
JHB <jhernandezb96@gmail.com>
2022-01-30 12:39:57 -0500

    StrawberryVibration: +validation before vibration

--- lib/src/a_utils/strawberry_vibration.dart
+++ lib/src/a_utils/strawberry_vibration.dart
@@ -1,17 +1,25 @@
 import 'package:vibration/vibration.dart';
 
 class StrawberryVibration {
-  static Future<void> vibrate({int duration = 500}) {
-    return Vibration.vibrate(
-      duration: duration,
-    );
+  static Future<void> vibrate({int duration = 500}) async {
+    //chequea si puede vibrar
+    //en dispositivos que no lo soporten, o el emulador, lanzan excepcion
+    if ((await Vibration.hasVibrator() ?? false) &&
+        (await Vibration.hasCustomVibrationsSupport() ?? false)) {
+      return Vibration.vibrate(
+        duration: duration,
+      );
+    }
   }
 
-  static Future<void> vibrateWithWinPattern({int duration = 7000}) {
-    return Vibration.vibrate(
-      pattern: [500, 500, 500, 1000],
-      intensities: [1, 255],
-      duration: duration,
-    );
+  static Future<void> vibrateWithWinPattern({int duration = 7000}) async {
+    if ((await Vibration.hasVibrator() ?? false) &&
+        (await Vibration.hasCustomVibrationsSupport() ?? false)) {
+      return Vibration.vibrate(
+        pattern: [500, 500, 500, 1000],
+        intensities: [1, 255, 1, 255],
+        duration: duration,
+      );
+    }
   }
 }
