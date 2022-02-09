1455562284d89bb341aa0dc925cfe04619db1742
Aidyl <jgarciaalbalah@gmail.com>
2022-02-09 18:23:04 -0500

    CommonsSubLevelBuilder: +size from mediaQuery

--- lib/src/c_commons/all/commons_sublevel_builder.dart
+++ lib/src/c_commons/all/commons_sublevel_builder.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:citmatel_strawberry_tools/src/a_utils/strawberry_custom_app_bar.dart';
 import 'package:citmatel_strawberry_tools/src/c_commons/all/commons_all_exporter.dart';
 import 'package:flutter/material.dart';
@@ -6,16 +7,9 @@ class CommonsSubLevelBuilder {
   static Scaffold buildScaffold({
     required int nivel,
     required String tema,
-    TextStyle nivelTextStyle = const TextStyle(
-      fontWeight: FontWeight.bold,
-      color: Colors.white,
-      fontSize: 20,
-    ),
-    TextStyle temaTextStyle = const TextStyle(
-      fontWeight: FontWeight.bold,
-      color: Colors.white,
-      fontSize: 25,
-    ),
+    required Size size,
+    TextStyle? nivelTextStyle,
+    TextStyle? temaTextStyle,
     int stars = 1,
     int maxStar = 3,
     Widget? body,
@@ -24,28 +18,53 @@ class CommonsSubLevelBuilder {
       backgroundColor: Colors.transparent,
       appBar: StrawberryCustomAppBar(
         changeOrderOfTheChilds: true,
-        smallChild: const Center(
-          child: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
+        smallChild: Center(
+          child: SizedBox(
+            width: size.width / 11,
+            height: size.width / 13,
+            child: const BackButton(
+              color: Colors.white,
+            ),
           ),
         ),
-        largeChild: Row(
-          children: [
-            Text(
-              "# $nivel",
-              style: nivelTextStyle,
-            ),
-            Text(
-              tema,
-              style: temaTextStyle,
-            ),
-            CommonsStarsIndicator(
-              stars: stars,
-              maxStars: maxStar,
-            ),
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        largeChild: SizedBox(
+          width: size.width / 1,
+          height: size.width / 13,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              AutoSizeText(
+                "#$nivel ",
+                style: nivelTextStyle ??
+                    TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: size.width / 20,
+                    ),
+                maxLines: 1,
+              ),
+              SizedBox(
+                width: size.width / 4,
+                child: AutoSizeText(
+                  "$tema  ",
+                  style: temaTextStyle ??
+                      TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: size.width / 16,
+                      ),
+                  maxLines: 1,
+                ),
+              ),
+              CommonsStarsIndicator(
+                stars: stars,
+                maxStars: maxStar,
+                normalSize: size.width / 14,
+                bigSize: size.width / 9,
+              ),
+            ],
+          ),
         ),
       ),
       body: body,
