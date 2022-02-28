5cfa3255843353c41b05ef70972cb1847b4f0e52
JHB <jhernandezb96@gmail.com>
2022-02-28 18:10:58 -0500

    StrawberryFunction: +timer & +dispose

--- lib/src/a_utils/strawberry_function.dart
+++ lib/src/a_utils/strawberry_function.dart
@@ -1,8 +1,16 @@
+import 'dart:async';
+
 import 'package:citmatel_strawberry_tools/tools_exporter.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class StrawberryFunction {
+  static Timer? _timer;
+
+  static dispose() {
+    _timer?.cancel();
+  }
+
   // This show a screen that tell to user it lost.
   static void looseLevel({
     Transition transition = Transition.zoom,
@@ -18,25 +26,29 @@ class StrawberryFunction {
   }) async {
     StrawberryAudio.playAudioLose(mute);
     // Time before push one page above other.
-    await Future.delayed(timeBeforeTheTransition);
-    Get.off(
-      // Go to a determinated page.
-      StrawberryLevelLose(
-        // The list of texts an is animation that the screen is going to show.
-        childFirstText: childFirstText,
-        // The Function that is going to be executed when the left button is pressed.
-        leftButtonFunction: leftButtonFunction,
-        // The Function that is going to be executed when the right button is pressed.
-        rightButtonFunction: rightButtonFunction,
-        // The image thas is going to go in the background.
-        backgroundImage: backgroundImage,
-        maxStar: maxStar,
-        stars: stars,
-      ),
-      // The transition of one page to another.
-      transition: transition,
-      // The duration of the transition.
-      duration: duration,
+    _timer = Timer(
+      timeBeforeTheTransition,
+      () {
+        Get.off(
+          // Go to a determinated page.
+          StrawberryLevelLose(
+            // The list of texts an is animation that the screen is going to show.
+            childFirstText: childFirstText,
+            // The Function that is going to be executed when the left button is pressed.
+            leftButtonFunction: leftButtonFunction,
+            // The Function that is going to be executed when the right button is pressed.
+            rightButtonFunction: rightButtonFunction,
+            // The image thas is going to go in the background.
+            backgroundImage: backgroundImage,
+            maxStar: maxStar,
+            stars: stars,
+          ),
+          // The transition of one page to another.
+          transition: transition,
+          // The duration of the transition.
+          duration: duration,
+        );
+      },
     );
   }
 
@@ -55,26 +67,30 @@ class StrawberryFunction {
   }) async {
     StrawberryAudio.playAudioWin(mute);
     // Time before push one page above other.
-    await Future.delayed(timeBeforeTheTransition);
-    StrawberryVibration.vibrateWithWinPattern();
-    Get.off(
-      // Go to a determinated page.
-      StrawberryLevelWin(
-        // The first list of texts an is animation that the screen is going to show.
-        childFirstText: childFirstText,
-        // The Function that is going to be executed when the left button is pressed.
-        leftButtonFunction: leftButtonFunction,
-        // The Function that is going to be executed when the right button is pressed.
-        rightButtonFunction: rightButtonFunction,
-        // The image thas is going to go in the background.
-        backgroundImage: backgroundImage,
-        maxStar: maxStar,
-        stars: stars,
-      ),
-      // The transition of one page to another.
-      transition: transition,
-      // The duration of the transition.
-      duration: duration,
+    _timer = Timer(
+      timeBeforeTheTransition,
+      () {
+        StrawberryVibration.vibrateWithWinPattern();
+        Get.off(
+          // Go to a determinated page.
+          StrawberryLevelWin(
+            // The first list of texts an is animation that the screen is going to show.
+            childFirstText: childFirstText,
+            // The Function that is going to be executed when the left button is pressed.
+            leftButtonFunction: leftButtonFunction,
+            // The Function that is going to be executed when the right button is pressed.
+            rightButtonFunction: rightButtonFunction,
+            // The image thas is going to go in the background.
+            backgroundImage: backgroundImage,
+            maxStar: maxStar,
+            stars: stars,
+          ),
+          // The transition of one page to another.
+          transition: transition,
+          // The duration of the transition.
+          duration: duration,
+        );
+      },
     );
   }
 }
